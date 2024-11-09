5784ec5c8734c0b13633c1a900655cfbd09723dd
Han KyuHyun <khh5345@naver.com>
2024-11-09 11:01:24 +0900

    vue 프로젝트 설정

--- vueedu/cdn-vue/components/Comp1.js
+++ vueedu/cdn-vue/components/Comp1.js
@@ -3,7 +3,7 @@ export default {
             <hr>
             <ul>
               <li>올라프</li>
-              <i>안나</li>
+              <li>안나</li>
               <li>엘사</li>
               <li>크리스토퍼</li>
             </ul>`
